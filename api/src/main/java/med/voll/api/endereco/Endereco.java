d366f1527e8b6ad096c809ff33208e474c0d9e04
Matheus dos Santos <matheusdossantos3157@gmail.com>
2024-03-19 18:44:33 -0300

    feat: adicionando o endpoint PUT

--- api/src/main/java/med/voll/api/endereco/Endereco.java
+++ api/src/main/java/med/voll/api/endereco/Endereco.java
@@ -28,4 +28,28 @@ public class Endereco {
         this.cidade = dados.cidade();
         this.uf = dados.uf();
     }
+
+    public void atualizarInformaces(DadosEndereco dados) {
+        if (this.logradouro != null){
+            this.logradouro = dados.logradouro();
+        }
+        if (this.bairro != null){
+            this.bairro = dados.bairro();
+        }
+        if (this.cep != null){
+            this.cep = dados.cep();
+        }
+        if (this.numero != null){
+            this.numero = dados.numero();
+        }
+        if (this.complemento != null){
+            this.complemento = dados.complemento();
+        }
+        if (this.cidade != null){
+            this.cidade = dados.cidade();
+        }
+        if (this.uf != null){
+            this.uf = dados.uf();
+        }
+    }
 }
--- api/src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
+++ api/src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
@@ -4,10 +4,11 @@ import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Especialidade;
 
 public record DadosListagemPaciente(
+        Long id,
         String nome,
         String email,
         String cpf) {
     public DadosListagemPaciente(Paciente paciente){
-        this(paciente.getNome(), paciente.getEmail(), paciente.getCpf());
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getCpf());
     }
 }
