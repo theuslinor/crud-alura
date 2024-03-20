67f8bb514f57e2cc5e64fb5931d2d48b6cdbfb3b
Matheus dos Santos <matheusdossantos3157@gmail.com>
2024-03-20 18:46:42 -0300

    feat: adicionando spring security

---
+++ api/src/main/java/med/voll/api/domain/usuario/AutenticacaoService.java
@@ -0,0 +1,19 @@
+package med.voll.api.domain.usuario;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AutenticacaoService implements UserDetailsService {
+
+    private final UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
+}
